20e81af0239e5c9d752c18864ccc32e989ba9e60
Tomas Hozza <thozza@redhat.com>
2015-10-01 14:34:10 +0200

    Use PBR for packaging
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- setup.py
+++ setup.py
@@ -16,83 +16,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Authors:
-
-import subprocess
-import os
-
-try:
-    from setuptools import setup, Command
-except ImportError:
-    from distutils.core import setup, Command
 
 
-class PyTest(Command):
-    user_options = [('test-runner=',
-                     't',
-                     'test runner to use; by default, multiple py.test runners are tried')]
-    command_consumes_arguments = True
-
-    def initialize_options(self):
-        self.test_runner = None
-        self.args = []
-
-    def finalize_options(self):
-        pass
-
-    def runner_exists(self, runner):
-        syspaths = os.getenv('PATH').split(os.pathsep)
-        for p in syspaths:
-            if os.path.exists(os.path.join(p, runner)):
-                return True
-
-        return False
-
-    def run(self):
-        # only one test runner => just run the tests
-        supported = ['2.7', '3.3']
-        potential_runners = ['py.test-' + s for s in supported]
-        if self.test_runner:
-            potential_runners = [self.test_runner]
-        runners = [pr for pr in potential_runners if self.runner_exists(pr)]
-
-        for runner in runners:
-            if len(runners) > 1:
-                print('\n' * 2)
-                print('Running tests using "{0}":'.format(runner))
-
-            cmd = [runner]
-            for a in self.args:
-                cmd.append(a)
-            cmd.append('-v')
-            cmd.append('test')
-            t = subprocess.Popen(cmd)
-            t.wait()
-
-        raise SystemExit(t.returncode)
-
-
-install_requires = []
-version = '0.1'
+from setuptools import setup
 
 setup(
-    name='Broken DNS Proxy',
-    version=version,
-    description='Simple DNS proxy for simulating DNS issues',
-    keywords='simulating,issues,dns,proxy',
-    url='https://github.com/thozza/broken-dns-proxy',
-    license='GPLv3+',
-    packages=['broken_dns_proxy'],
-    include_package_data=True,
-    entry_points={'console_scripts': ['bdp=broken_dns_proxy.cli_runner:CliRunner.run']},
-    install_requires=install_requires,
-    setup_requires=[],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Environment :: Console',
-                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-                 'Operating System :: POSIX :: Linux',
-                 'Programming Language :: Python',
-                 ],
-    cmdclass={'test': PyTest}
+    setup_requires=['pbr'],
+    pbr=True,
 )
